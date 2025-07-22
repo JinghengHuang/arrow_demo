e4be423fc84eab454bfd5e0937385e69df4361d9
yixing <leiyixing@gmail.com>
2025-07-22 11:35:17 +0100

    feat: use factory mode for data models

--- src/objects/solver_model.py
+++ src/objects/solver_model.py
@@ -6,9 +6,9 @@
 from objects.base_model import ArrowModel
 
 
-class SolverConfig(ArrowModel):
+class SolverModel(ArrowModel):
     """
-    SolverConfig: Configuration for the optimization solver
+    SolverModel: Configuration for the optimization solver
     This class provides a way to define the solver configuration, including the solver name,
     solver type, and any additional parameters needed for the solver.
     It inherits from ArrowModel to ensure compatibility with Arrow's in-memory format.
